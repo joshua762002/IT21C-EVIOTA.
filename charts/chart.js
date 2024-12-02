14aa1032a9493f8369bf49abe83c3cde9196d30b
joshua762002 <joshuaeviota60@gmail.com>
2024-12-02 20:26:31 +0800

    Added a create are chart

--- charts/chart.js
+++ charts/chart.js
@@ -36,3 +36,37 @@ class LineChart extends ChartCreator {
     createCharts() {
         this.createAreaChart();
     }
+class LineChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.areaCtx = document.getElementById('areaChart');
+    }
+  
+    createCharts() {
+        this.createAreaChart();
+    }
+    createAreaChart() {
+        new Chart(this.areaCtx, {
+            type: 'line',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: this.chartData.data,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    fill: true,
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+  }
+  
