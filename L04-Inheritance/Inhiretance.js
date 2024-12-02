86f3a74f3e2da9d0e3777c27ea24aab7ff246fd1
joshua762002 <joshuaeviota60@gmail.com>
2024-12-02 12:07:23 +0800

    Added Class person

---
+++ L04-Inheritance/Inhiretance.js
@@ -0,0 +1,11 @@
+class Person {
+    #name;
+    #age;
+    #status;
+  
+    constructor(name, age, status) {
+      this.#name = name;
+      this.#age = age;
+      this.#status = status;
+    }
+}
