f9689cc4b61d24b54c626ecf25a88147fd5d9110
joshua762002 <joshuaeviota60@gmail.com>
2024-12-02 10:45:56 +0800

    Added js

---
+++ Objects And Classes/topic-1.js
@@ -0,0 +1,10 @@
+const person1 ={
+    name:"joshua eviota",
+    age:22,
+    occupation:"student nga hilomon"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occup);
