2cf9a23646c3fb94f50378fc9e45f2409eb4b81c
joshua762002 <joshuaeviota60@gmail.com>
2024-12-02 20:23:52 +0800

    Added a class linechart

--- charts/chart.js
+++ charts/chart.js
@@ -27,3 +27,12 @@ createCharts() {
     throw new Error('createCharts() must be implemented in subclasses');
 }
 }
+class LineChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.areaCtx = document.getElementById('areaChart');
+    }
+}
+    createCharts() {
+        this.createAreaChart();
+    }
